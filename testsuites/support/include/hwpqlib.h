cc175d8fabcb166c86f466b13b7e90de7862a41a
Gedare Bloom <gedare@rtems.org>
2012-03-01 11:31:01 -0500

    HWPQ: Add hwpqlib.

---
+++ testsuites/support/include/hwpqlib.h
@@ -0,0 +1,18 @@
+
+#include <libcpu/spillpq.h>     /* bad */
+#include <libcpu/unitedlistpq.h> /* bad */
+#include <libcpu/splitheappq.h> /* bad */
+
+typedef enum {
+  HWPQLIB_SPILLPQ_UNITEDLIST,
+  HWPQLIB_SPILLPQ_SPLITHEAP,
+  HWPQLIB_SPILLPQ_NONE
+} hwpqlib_spillpq_t;
+
+/* Public interface for pqbench */
+void hwpqlib_initialize( hwpqlib_spillpq_t type, int pq_id, int size );
+void hwpqlib_insert( int pq_id, int key, int value );
+uint64_t hwpqlib_first( int pq_id );
+uint64_t hwpqlib_pop( int pq_id );
+
+
---
+++ testsuites/support/src/hwpqlib.c
@@ -0,0 +1,47 @@
+
+#include <hwpqlib.h>
+
+static inline void unitedlist_initialize( int id, int size ) {
+  spillpq_ops = &sparc64_unitedlistpq_ops;
+  sparc64_spillpq_initialize(id, size);
+}
+
+static inline void splitheap_initialize( int id, int size ) {
+  spillpq_ops = &sparc64_splitheappq_ops;
+  sparc64_spillpq_initialize(id, size);
+}
+
+void hwpqlib_initialize( hwpqlib_pq_t type, int hwpq_id, int size ) {
+  switch(type) {
+    case hwpq_unitedlist:
+      unitedlist_initialize(hwpq_id, size);
+      break;
+
+    case hwpq_splitheap:
+      splitheap_initialize(hwpq_id, size);
+      break;
+
+    default:
+      break;
+  }
+}
+
+void hwpqlib_insert( int pq_id, int key, int value ) {
+  HWDS_ENQUEUE(pq_id, key, value);
+}
+
+uint64_t hwpqlib_first( int pq_id ) {
+  uint64_t kv;
+  HWDS_FIRST(pq_id, kv);
+  return kv;
+}
+
+uint64_t hwpqlib_pop( int pq_id ) {
+  uint64_t kv;
+  // TODO: why not just one op for pop?
+  HWDS_FIRST(pq_id, kv);
+  if ( kv != (uint64_t)-1 )
+    HWDS_EXTRACT(pq_id, kv);  
+  return kv;
+}
+
