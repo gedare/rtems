5ed1e4ab9a8ee1eb3fe53e9c3319b2012d567be0
Gedare Bloom <gedare@rtems.org>
2012-03-04 14:35:31 -0500

    hwpq: rewrite exceptions to make hwpq state sane.

--- c/src/lib/libcpu/sparc64/shared/interrupts/hwpq_exceptions.c
+++ c/src/lib/libcpu/sparc64/shared/interrupts/hwpq_exceptions.c
@@ -14,6 +14,12 @@ void sparc64_hwpq_initialize()
    );
 
   _CPU_ISR_install_vector(
+    SPARC_ASYNCHRONOUS_TRAP(0x42),
+    sparc64_hwpq_exception_handler,
+    &old
+   );
+
+  _CPU_ISR_install_vector(
     SPARC_ASYNCHRONOUS_TRAP(0x43),
     sparc64_hwpq_exception_handler,
     &old
@@ -56,55 +62,42 @@ void sparc64_hwpq_exception_handler(
 
   trap_context = (uint32_t)context;
   trap_idx = ((trap_context)&(~0))>>20; // what is trying to be used?
-  trap_operation = (trap_context)&~(~0 << (3 + 1)); // what is the op?
+//  trap_operation = (trap_context)&~(~0 << (3 + 1)); // what is the op?
 
   /* handle the context switch first */
-  /* TODO: defer / deny context switch?? */
-  if ( trap_idx != queue_idx ) {
 
-    if ( trap_operation == 3 ) {
+  switch (softint_bit) {
+    case 1: // need_spill
+      sparc64_spillpq_handle_spill(
+          queue_idx,
+          spillpq_queue_max_size[queue_idx]/2
+      );
+      break;
+    case 2: // need_fill
+      sparc64_spillpq_handle_fill(
+          queue_idx,
+          spillpq_queue_max_size[queue_idx]/2
+      );
+      break;
+    case 3: { // soft_extract
       uint64_t kv;
       HWDS_GET_PAYLOAD(kv);
       sparc64_spillpq_handle_extract(queue_idx, kv);
+      break;
     }
-    sparc64_spillpq_context_switch(queue_idx);
-
-    HWDS_SET_CURRENT_ID(trap_idx);
-    sparc64_spillpq_handle_fill(trap_idx, spillpq_queue_max_size[trap_idx]/2);
-    sparc_enable_interrupts(level);
-    return;
-  }
-
-  // FIXME: allow control over how much to spill/fill?
-  switch (trap_operation) {
-    case 2: //enqueue
-      sparc64_spillpq_handle_spill(queue_idx, spillpq_queue_max_size[queue_idx]/2);
-      break;
-
-    case 3: //extract
-      if ( softint_bit == 3 ) {
-        uint64_t kv;
-        HWDS_GET_PAYLOAD(kv);
-        if ( !sparc64_spillpq_handle_extract(queue_idx, kv) )
-          HWDS_CHECK_UNDERFLOW(queue_idx);
-      } else {
-        sparc64_spillpq_handle_fill(queue_idx, spillpq_queue_max_size[queue_idx]/2);
-      }
-      break;
-
-    case 6: //extract_last
-      sparc64_spillpq_handle_fill(queue_idx, spillpq_queue_max_size[queue_idx]/2);
-      break;
-
-    case 11: //check_underflow
-      sparc64_spillpq_handle_fill(queue_idx, spillpq_queue_max_size[queue_idx]/2);
+    case 4: // context_switch
+      sparc64_spillpq_context_switch(queue_idx);
+      HWDS_SET_CURRENT_ID(trap_idx);
+      sparc64_spillpq_handle_fill(
+          trap_idx,
+          spillpq_queue_max_size[trap_idx]/2
+      );
       break;
 
     default:
-
+      printk("Invalid softint hwpq exception\n");
       break;
   }
-  
+
   sparc_enable_interrupts(level);
 }
-
